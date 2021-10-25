5c585b3eeefeca279e881c7f30a1085b0ae24bcf
Alex Thomason <alexthomason28@gmail.com>
2021-10-25 01:26:59 -0400

    energy_saver_algorthm.py

---
+++ energy_saver_algorthm.py
@@ -0,0 +1,19 @@
+# energy_saver_algorthm.py
+# Author: Alex Thomason
+
+
+# Import necessary packages
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+
+# Begin Modular Function Code
+def main():
+    """
+    This function will drive the energy saving algorithm
+    """
+    statement = "complete"
+
+
+if __name__ == "__main__":
+    statement = main()
