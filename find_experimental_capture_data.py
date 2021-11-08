351143b662b5bebafda78968287ed793565b808a
Alex Thomason <alexthomason28@gmail.com>
2021-11-07 21:55:55 -0500

    find_experimental_capture_data.py - edited find_capture_voltage() function to be able to detect if the start voltage was below capture threshold. Also added logging to display final capture voltage. Also added a docstring to find_capture_voltage() function. Also modified the algorithm driver() function to now be find_patient_capture_voltage() function

--- find_experimental_capture_data.py
+++ find_experimental_capture_data.py
@@ -23,20 +23,58 @@ def detect_capture(voltage_list, capture_list, trial_voltage):
         return True"""
 
 
-def find_capture_voltage(duration_list, voltage_list, capture_list):
+def find_capture_voltage(duration_list: list, voltage_list: list, capture_list: list):
+    """Finds the capture voltage of a patient at a certain stimulus duration.
+
+    This function contains the algorithm to find the capture voltage
+    threshold of a patients myocardial tissue. In real life, a
+    pacemaker could use a version of this algrithm to find the stimuls
+    thresholds at various stimulation pulse durations. This function
+    detects if a voltage has been captured by viewing the capture status
+    of the previously generated data. Reference the module
+    "generate_capture_data.py" to see how that data is generated.
+    In a real pacemaker, a capture detection algorithm of the heart's
+    electrical signals would be used to determine whether or not the
+    tissue has been captured.
+
+    Summary of how this function works:
+        - Start voltage is pre-determined to be 3 V
+        - no_capture_counter is set to 0
+        - Other relavent variables are pre-determined (see # descriptions)
+        - While loop initiated --> continues to run as long as
+                the no_capture_counter is less than 2
+        - If the starting voltage is not strong enough to stimulate the
+                myocardium, then the experimental voltage is set to 5 V
+        - If the experimental voltage (or starting voltage) is not
+                enough to stimulate the myocardium, then a backup
+                voltage of 5V is given to the patient to ensure
+                that pacing occurs.
+        - Finds returns the capture voltage of the myocardial tissue
+                within a 5% error above the true cpature voltage
+
+    Args:
+        duration_list (list): list of constant pulse durations
+        voltage_list (list): stimulus voltage amplitude that is the
+                             varying voltage of the stimulus
+        capture_list (list): List of Capture Status values coresponding
+                             to the duration_list and voltage_list
+                             (1 = capture, 0 = no capture)
+
+    Returns:
+        capture_voltage (float): capture voltage of the myocardial tissue
+                within a 5% error above the true cpature voltage
     """
-    
-    """
-    voltage_start = 3           # [V] Start voltage
+    # [V] Start voltage
+    voltage_start = 3
+    # [V] Experimental voltage and itterated in while loop
     voltage_experimental = voltage_start
+    # [V] List to store all the voltages that capture the myocardium
     capture_voltage_experimental_list = []
+    # Counter to keep track of the number of times an experimental volgate
+    # does not capture the myocardium
     no_capture_counter = 0
-    # small_step_indicator indicates voltage step size.
-    # 0.75% voltage step when small_step_indicator = 0
-    # 0.95% voltage step when small_step_indicator = 1
+    # Indicator to decrease the experimental voltage in smaller steps
     small_step_indicator = 0
-#    step_down_size = 0.25 * voltage_experimental
-#    small_step_up_size = 0.05
 
     while no_capture_counter < 2:
         idx, voltage_experimental = gcd.find_nearest(voltage_list,
@@ -47,25 +85,25 @@ def find_capture_voltage(duration_list, voltage_list, capture_list):
             logging.info("Captured: Myocardial tissue was captured with \
 stimulus of {} V for {} ms".format(voltage_experimental, duration_list[0]))
             capture_voltage_experimental_list.append(voltage_experimental)
-            
+
             if small_step_indicator == 0:
                 voltage_experimental *= 0.75
-            
+
             if small_step_indicator == 1:
                 voltage_experimental *= 0.95
-        
+
         if capture_status == 0:
             logging.info("Failed to Capture: Myocardial tissue was not \
 captured with stimulus of {} V for {} ms. A backup pulse of 4.5 V was \
 applied to the patient".format(voltage_experimental, duration_list[0]))
-            
+
             # Increases the voltage to 5 Volts if the beginning voltage is
             # insufficient for myocardial stimulation
             if len(capture_voltage_experimental_list) == 0:
                 logging.info("Start voltage of {} V did not stimulate \
 the myocardial tissue at at stimulation duration of {} ms. The next \
 stimulus voltage is set to be 5 V.".format(voltage_experimental,
-duration_list[0]))
+                                           duration_list[0]))
                 voltage_experimental = 5
                 continue
 
@@ -73,7 +111,7 @@ duration_list[0]))
                 no_capture_counter += 1
                 voltage_experimental = capture_voltage_experimental_list[-1]
                 small_step_indicator = 1
-                
+
     print("Capture Voltage Experimental List: \
 {}".format(capture_voltage_experimental_list))
     logging.info("Capture Voltage Experimental List: \
@@ -88,7 +126,22 @@ is: {}".format(capture_voltage))
     return capture_voltage
 
 
-def algorithm_driver(filename):
+def find_patient_capture_voltage(filename:str):
+    """Finds the capture voltage of a patient data file
+
+    Args:
+        filename (str): patient data file ending in .csv
+
+        Note: The patient data should have the following columns:
+        (1) stimulus duration - constant pulse duration
+        (2) Stimulus Voltage Amplitude - varying voltage of the stimulus
+        (3) Capture Status (1 = capture, 0 = no capture)
+    
+    Returns:
+        capture_voltage (float): capture voltage of the myocardial tissue
+                for a pulse duration (duration value is specified in the
+                first column of the patient data file)
+    """
     duration_list, voltage_list, capture_list = icd.import_parse_convert_data(
                                                 filename)
     logging.basicConfig(filename="log_files/{}.log".format(
@@ -96,9 +149,9 @@ def algorithm_driver(filename):
                         level=logging.INFO)
     capture_voltage = find_capture_voltage(duration_list, voltage_list,
                                            capture_list)
-    print(capture_voltage)
+    return capture_voltage
 
 
 if __name__ == "__main__":
-    filename = "patient1_0.2ms.csv"
+    filename = "patient1_0.3ms.csv"
     algorithm_driver(filename)
